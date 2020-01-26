a7efdc8f28b62d94ec553ebaf8772d88b984ae03
Crapo <vincent@girol.fr>
2020-01-26 02:02:16 +0100

    Added versions

---
+++ src/Versions.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VGirol\JsonApiConstant;
+
+/**
+ * All the available versions of the specification
+ */
+abstract class Versions
+{
+    public const VERSION_1_0 = '1.0';
+    public const VERSION_1_1 = '1.1';
+}
